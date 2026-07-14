9b8acbebd9443a41e67af1efab28d4daaf2a4ce7
TristanLira <24030458@itcelaya.edu.mx>
2026-07-14 11:13:57 -0600

    arreglar matrices de transformarciones

--- geometry/Transformations.lua
+++ geometry/Transformations.lua
@@ -40,86 +40,84 @@ end
 
     -- Matriz de traslacion 
 function t:translate_matrix(tx, ty, tz)
-    return Matrix
-    (1, 0, 0, tx)
-    (0, 1, 0, ty)
-    (0, 0, 1, tz)
-    (0, 0, 0, 1)
-    
+    return Matrix({
+        {1, 0, 0, tx},
+        {0, 1, 0, ty},
+        {0, 0, 1, tz},
+        {0, 0, 0, 1}
+    })
 end
 
     -- Matriz de escalamiento
 function t:escale_matrix(sx, sy, sz)
-    return Matrix
-    (sx, 0, 0, 0)
-    (0, sy, 0, 0)
-    (0, 0, sz, 0)
-    (0, 0, 0, 1)
-    
+    return Matrix({
+        {sx, 0, 0, 0},
+        {0, sy, 0, 0},
+        {0, 0, sz, 0},
+        {0, 0, 0, 1}
+    })
 end
 
     -- Matriz de rotacion
 function t:rotateXY_matrix(angle)
     local sin = math.sin(angle)
     local cos = math.cos(angle)
-    return Matrix(
-    {cos, -sin, 0, 0},
-    {sin, cos, 0, 0}, 
-    {0, 0, 1, 0}, 
-    {0, 0, 0, 1})
-    
+    return Matrix({
+        {cos, -sin, 0, 0},
+        {sin, cos, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}
+    })
 end
 
 function t:rotateXZ_matrix(angle)
     local sin = math.sin(angle)
     local cos = math.cos(angle)
-    return Matrix(
-    {cos, 0, sin, 0},
-    {0, 1, 0, 0}, 
-    {-sin, 0, cos, 0}, 
-    {0, 0, 0, 1})
-    
+    return Matrix({
+        {cos, 0, sin, 0},
+        {0, 1, 0, 0},
+        {-sin, 0, cos, 0},
+        {0, 0, 0, 1}
+    })
 end
 
 function t:rotateYZ_matrix(angle)
     local sin = math.sin(angle)
     local cos = math.cos(angle)
-    return Matrix(
-    {1, 0, 0, 0},
-    {0, cos, -sin, 0}, 
-    {0, sin, cos, 0}, 
-    {0, 0, 0, 1})
-    
+    return Matrix({
+        {1, 0, 0, 0},
+        {0, cos, -sin, 0},
+        {0, sin, cos, 0},
+        {0, 0, 0, 1}
+    })
 end
 
     -- Matriz de sesgado
 function t:ShearXY_matrix(shxz, shyz)
-    return Matrix (
+    return Matrix ({
         {1, 0, shxz, 0},
         {0, 1, shyz, 0},
         {0, 0, 1, 0},
         {0, 0, 0, 1}
-    )
-    
+    })
 end
 
 function t:ShearXZ_matrix(shxy, shzy)
-    return Matrix (
+    return Matrix ({
         {1, shxy, 0, 0},
         {0, 1, 0, 0},
         {0, shzy, 1, 0},
         {0, 0, 0, 1}
-    )
-    
+    })
 end
 
 function t:ShearYZ_matrix(shyx, shzx)
-    return Matrix (
+    return Matrix ({
         {1, 0, 0, 0},
         {shyx, 1, 0, 0},
         {shzx, 0, 1, 0},
         {0, 0, 0, 1}
-    )
+    })
     
 end
 
